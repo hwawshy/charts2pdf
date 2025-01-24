3cf7d0c8352005113592d3f8d59799c9d0641a3b
muhammad <m.elhwawshy@gmail.com>
2025-01-24 12:53:38 +0100

    add logo

--- app/assets/component/App.tsx
+++ app/assets/component/App.tsx
@@ -1,11 +1,12 @@
 import {Editor} from "./editor/Editor.tsx";
+import Logo from '../icons/logo.svg';
 
 export type Props = object
 
 
 export function App() {
     return <>
-        <h1 className={"text-center font-medium text-2xl text-gray-800 mt-2.5"}>ChartToPdf</h1>
+        <Logo className={'mx-auto w-[300px]'} />
         <Editor />
     </>;
 }
